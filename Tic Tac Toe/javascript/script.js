aeb1903d6f1991078035d1f4de11a265ee7d7037
Samiksha Chandak <schandak@tavisca.com>
2017-08-28 12:22:49 +0530

    tic tac toe using jquery

---
+++ Tic Tac Toe/javascript/script.js
@@ -0,0 +1,144 @@
+var player1Name="" , player2Name="", turn = "";
+var grid =  [[0,0,0],[0,0,0],[0,0,0]];
+var hasWinner = 0, moveCount=0;
+
+function boardMessage(x){
+    return $("#board").text(x);
+}
+
+function setTurn(){
+    var r = Math.floor((Math.random() * 2) + 1);
+    hasWinner=0;
+    if(r==1){
+        turn = player1Name;
+        boardMessage(player1Name+"'s turn now!");
+    }
+    else{
+        turn = player2Name;
+        boardMessage(player2Name+"'s turn now!");
+    }}
+
+function init(){
+        turn = "";
+        grid =  [[0,0,0],[0,0,0],[0,0,0]];
+        boardMessage("");
+        $(".col").map(function() {
+            $(this).text("");
+        }).get();
+        hasWinner = 0;
+        moveCount=0;}
+
+$("#playButton").click(function (){
+
+    if(hasWinner==1){
+        init();
+    }
+
+    player1Name = $("#player-1-inp").val();
+    player2Name = $("#player-2-inp").val();
+
+    if(player1Name=="" || player2Name==""){
+        alert("Please Set Player Names");
+        return;
+    }
+
+    setTurn();});
+
+$("#resetButton").click(function(){
+		turn = "";
+        grid =  [[0,0,0],[0,0,0],[0,0,0]];
+        boardMessage("");
+        $(".col").map(function() {
+            $(this).text("");
+        }).get();
+        hasWinner = 0;
+        moveCount=0;});
+
+$(".col").click(function (){
+
+    if(player1Name=="" || player2Name==""){
+        alert("Please Set Name Of Players");
+        return;
+    }
+
+    var row = $(this).parent().index();
+    var col = $(this).index();
+
+    if(grid[row][col]!==0){
+        alert("Please Try Other Position");
+        return;
+    }
+    if(hasWinner==1){
+        alert("Click Play Again");
+        return;
+    }
+
+    if(turn==player1Name){
+        moveCount++;
+        $(this).text("O");
+        grid[row][col] = 1;
+        var ifWon = winnerCheck(1,player1Name);
+        if(!ifWon){
+            if(moveCount>=9){
+                boardMessage("Match Drawn!");
+                moveCount=0;
+                $("#playButton").text("Play again");
+                hasWinner=1;
+                return;
+            }else{
+                turn = player2Name;
+                boardMessage(player2Name+"'s turn now!");
+            }
+            return;    
+        }
+        else{
+            return;
+        }        
+    }
+    else if(turn==player2Name){
+        moveCount++;
+        $(this).text("X");
+        grid[row][col] = 2;
+        var ifWon = winnerCheck(2,player2Name);
+        if(!ifWon){
+            if(moveCount>=9){
+                boardMessage("Match Drawn!");
+                moveCount=0;
+                $("#playButton").text("Play again");
+                hasWinner=1;
+                return;
+            }else{
+                turn = player1Name;
+                boardMessage(player1Name+"'s turn now!");
+            }
+            return;    
+        }
+        else{
+            return;
+        }        
+    }});
+
+function winnerCheck(n,playerName){
+    if(
+
+        (grid[0][0]==n && grid[0][1]==n && grid[0][2]==n) ||
+        (grid[1][0]==n && grid[1][1]==n && grid[1][2]==n) ||
+        (grid[2][0]==n && grid[2][1]==n && grid[2][2]==n) ||
+
+        (grid[0][0]==n && grid[1][0]==n && grid[2][0]==n) ||
+        (grid[0][1]==n && grid[1][1]==n && grid[2][1]==n) ||
+        (grid[0][2]==n && grid[1][2]==n && grid[2][2]==n) ||
+
+        (grid[0][0]==n && grid[1][1]==n && grid[2][2]==n)||
+        (grid[0][2]==n && grid[1][1]==n && grid[2][0]==n)
+
+
+        ){
+        boardMessage(playerName+" won the game!");
+        hasWinner = 1;
+        moveCount=0;
+        $("#playButton").text("Play again");
+        return true;
+    }
+    return false;
+}
